81f1e19d003a0ed9f308e6de9dce383d56a65a23
Minh Hiền Hydraulics <banhtungnguyen42@gmail.com>
2026-07-18 13:45:19 +0700

    Gắn URL Apps Script đã deploy vào js/config.js
    Kích hoạt đồng bộ sản phẩm, đơn hàng và yêu cầu tư vấn với Google Sheet.

--- js/config.js
+++ js/config.js
@@ -5,4 +5,4 @@
    Để trống "" thì website vẫn chạy bình thường với dữ liệu mẫu có sẵn.
    ========================================================= */
 
-const APPS_SCRIPT_URL = "";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxlIw5LuuphOdowghg2X1miDroAFYmTd55lccu9Qu7a8SGYRFKRK9jy5B0PNRFkPooNyQ/exec";
